480f31e59fb03742388aa7bb1e859667423243d4
Truong Khanh Huy <huychongxang@gmail.com>
2020-08-15 09:16:45 +0700

    required plugin

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+define('THEME_URL', get_stylesheet_directory_uri());
+define('THEME_DIR', get_stylesheet_directory());
+define('THEME_ASSET_WEB', get_stylesheet_directory_uri() . '/assets/web');
+
+/**
+ * Required Plugin
+ *  
+ *
+ */
+require_once(THEME_DIR . '/TGM-Plugin-Activation-develop/example.php');
