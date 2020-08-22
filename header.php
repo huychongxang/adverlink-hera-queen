e605302d90ef65e7b403327238335c294de029ed
Truong Khanh Huy <huychongxang@gmail.com>
2020-08-22 15:59:07 +0700

    no message

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./assets/images/logo.png" type="image/x-icon">
-    <title>Kiều Anh Hera</title>
     <?php wp_head(); ?>
 </head>
 
