c9fb09665eac9d6eed216e2a6a9c07198d73cf58
mioriaty <mioriaty@gmail.com>
2020-08-24 21:42:40 +0700

    reverse ajax"

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -91,3 +91,106 @@ document.addEventListener('DOMContentLoaded', function () {
   const deadline = new Date(Date.parse(new Date()) + 432000000);
   initializeClock('countdown', deadline);
 });
+
+jQuery(document).ready(function ($) {
+  $('#form-register').on('submit', function (e) {
+    e.preventDefault();
+
+    var $form = $(this);
+    var data = jQuery(this).serialize();
+    jQuery.ajax({
+      action: 'submit_register_course',
+      type: 'POST',
+      url: '/wp-admin/admin-ajax.php',
+      data: data,
+      success: function (data) {
+        var data = JSON.parse(data);
+        if (data.error) {
+          alert('Đăng ký thất bại');
+        } else {
+          alert('Đăng ký thành công');
+        }
+      },
+      fail: function (err) {
+        alert('Đăng ký thất bại');
+      },
+    });
+  });
+  var currentPage = 1;
+  var totalPage = 0;
+
+  function fetchImages(page = 1) {
+    jQuery.ajax({
+      type: 'GET',
+      url: '/wp-json/v1/images/get',
+      data: {
+        per_page: 6,
+        page: page,
+      },
+      success: function (data, textStatus, request) {
+        totalPage = data.total_pages;
+        var total = data.total;
+        var images = data.data;
+        currentPage = data.current_page;
+        renderImages(images);
+        renderPagination(totalPage);
+      },
+      fail: function (err) {},
+    });
+  }
+
+  var rootImage = $('#root-images');
+
+  function renderImages(images) {
+    var html = '';
+    for (var i = 0; i < images.length; i++) {
+      var image = images[i];
+      html += `<div class="col-md-6 col-lg-4">
+                      <a data-fancybox="gallery" href="${image.thumbnail}">
+                          <img class="img-fluid" src="${image.thumbnail}">
+                      </a>
+                  </div>`;
+    }
+    rootImage.html(html);
+  }
+
+  var rootPagination = $('#root-pagination');
+
+  function renderPagination(numberPage) {
+    var previousPage =
+      parseInt(currentPage) - 1 <= 0 ? 1 : parseInt(currentPage) - 1;
+    var html = `<li class="page-item">
+                                  <a class="page-link" value="${previousPage}" aria-label="Previous">
+                                      <span aria-hidden="true">&laquo;</span>
+                                      <span class="sr-only">Previous</span>
+                                  </a>
+                              </li>`;
+    for (var i = 1; i <= numberPage; i++) {
+      if (i == currentPage) {
+        html += `<li class="page-item active">
+                                  <p class="page-link">${i}</p>
+                              </li>`;
+      } else {
+        html += ` <li class="page-item"><a class="page-link" value="${i}">${i}</a></li>`;
+      }
+    }
+
+    var nextPage =
+      currentPage < totalPage ? parseInt(currentPage) + 1 : totalPage;
+    html += ` <li class="page-item">
+                                  <a class="page-link" value="${nextPage}" aria-label="Next">
+                                      <span aria-hidden="true">&raquo;</span>
+                                      <span class="sr-only">Next</span>
+                                  </a>
+                              </li>`;
+    rootPagination.html(html);
+  }
+
+  $('body').on('click', 'a.page-link', function (e) {
+    e.preventDefault();
+    var page = $(this).attr('value');
+    fetchImages(page);
+  });
+
+  fetchImages();
+});
